be049f48e887cf443dc4de1b3c145eb098e74209
github username <github email address>
2018-05-28 21:44:55 -0700

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+# Keys of Hash Lab
+# Laura Nadolski
+# May 28 2018
+
+# Returns an array with every key from the hash whose value matches the value(s)
+# given as an argument.
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*argument)
+    helper_array = []
+    key_array = self.keys
+    self.values.each_with_index do |value, index|
+      argument.each do |item|
+        if value == item
+          helper_array.push key_array[index]
+        end
+      end
+    end
+    return helper_array
   end
 end
